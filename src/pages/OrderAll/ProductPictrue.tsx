be79195b61f36720ae06efd1cb557d62d4f3ac17
yenru980922 <yenyen120886@gmail.com>
2024-04-07 13:41:39 +0800

    更新葉子購物車

--- src/pages/OrderAll/ProductPictrue.tsx
+++ src/pages/OrderAll/ProductPictrue.tsx
@@ -1,26 +1,29 @@
-import { useEffect } from "react";
-import { useGetApiProductsId } from "../../API";
-import noImage from "../../assets/images/noImage.jpg";
+import { useEffect } from 'react';
+import { useGetApiProductsId } from '../../API';
+import noImage from '../../assets/images/noImage.jpg';
+import './Picture.css';
 const ProductPictrue: React.FC<{ productId: number }> = ({ productId }) => {
   const productResponse = useGetApiProductsId(productId);
 
   return (
-    <div className="d-flex justify-content-center">
-      <img
-        src={
-          productResponse.data?.data?.imageUrl &&
-          productResponse.data?.data?.imageUrl[0]
-            ? productResponse.data?.data?.imageUrl[0]
-            : noImage
-        }
-        alt="book"
-        style={{
-          height: "50px%",
-          objectFit: "cover",
-          backgroundColor: "#FFFFFf",
-          backgroundSize: "contain",
-        }}
-      />
+    <div className='product-detail-box'>
+      <div className='img-block2'>
+        <img
+          src={
+            productResponse.data?.data?.imageUrl &&
+            productResponse.data?.data?.imageUrl[0]
+              ? productResponse.data?.data?.imageUrl[0]
+              : noImage
+          }
+          alt='book'
+          // style={{
+          //   // height: '120px',
+          //   objectFit: 'cover',
+          //   backgroundColor: '#FFFFFf'
+          //   // backgroundSize: 'contain'
+          // }}
+        />
+      </div>
     </div>
   );
 };
