cbb87b239c9807e146d7743124dd987654931ef8
snow <snow860408@gmail.com>
2024-04-08 13:19:45 +0800

    跳轉頁加背景

--- src/pages/UsedBooksOrderPage/UsedBooksOrderConfirmation.tsx
+++ src/pages/UsedBooksOrderPage/UsedBooksOrderConfirmation.tsx
@@ -1,6 +1,7 @@
 import { useLocation } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
 import usedBookLogo from '../../assets/images/logo_dark.png'
+import backgroundImage from '../../assets/images/backgroundImage2.jpg'
 
 const UsedBooksOrderConfirmation: React.FC = () => {
   const [paymentStatus, setPaymentStatus] = useState<boolean>(false);
@@ -19,12 +20,13 @@ const UsedBooksOrderConfirmation: React.FC = () => {
 
   return (
 
-    <div className="cart text-center">
-      <img style={{ width: '350px' }} className='mb-5' src={usedBookLogo} />
-      {paymentStatus ? <h3 className='mb-3'>付款成功，感謝您的購買</h3> :
-        <h3 className='m-4'>付款失敗</h3>}
-      <h5>請等候跳轉...</h5>
-    </div>
+    <div className="cart text-center" style={{ backgroundImage: `url(${backgroundImage})` }}>
+      <div>
+        <img style={{ width: '350px' }} className='my-5' src={usedBookLogo} />
+        <h1 className='my-5' style={{ color: 'rgb(196, 141, 70)', fontSize: '48px' }}> {paymentStatus ? "付款成功" : "付款失敗"}</h1>
+        <h5 className='mb-5' style={{ color: 'rgb(196, 141, 70)' }}>請等候跳轉...</h5>
+      </div>
+    </div >
 
   );
 };
